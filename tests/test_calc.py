df37acaf8fbcdd48a1e8638d765ef581cb3b66c8
Najlepszy Kursant <najlepszykursant@szkolenie.cicd>
2025-03-27 16:36:31 +0100

    test v14321

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,11 +1,19 @@
+import pytest
+
 def add(a,b):
     return a + b
 
 def test_add():
     assert add(2, 3) == 5
 
-def test_add2():
-    assert add(2, 3) == 6
+@pytest.mark.parametrize("a,b,result",[
+    (2, 5, 7),
+    (-1, 1, 0),
+    (-1, -18, -19)
+])
 
-def test_add3():
-    assert add(2, 3) == 18
+def test_param_add(a,b,result):
+    assert a + b == result
+
+# def test_add2():
+#     assert add(2, 3) == 6
