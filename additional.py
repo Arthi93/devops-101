6a3e81c9d95ed459c52ba162dfe6f4133d9fc27f
Najlepszy Kursant <najlepszykursant@szkolenie.cicd>
2025-03-27 14:44:18 +0100

    teraz bdize ok

--- additional.py
+++ additional.py
@@ -3,4 +3,4 @@ print("Hellow world! REDOother!")
 print("Here we go again.other..")
 print("Additional file to pr")
 
-# kolejny popsuty kod, chociaz raz poczuje sie jak Łukasz
+# kolejny popsuty kod, chociaz raz poczuje sie jak Łukaszek biedny
